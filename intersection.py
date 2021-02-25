4b4af042ab29d63a9114240700093ac639c349ff
Jonathan Pan <jonathanpan@berkeley.edu>
2021-02-25 12:07:06 -0800

    first iter

--- intersection.py
+++ intersection.py
@@ -1,40 +1,39 @@
+import numpy as np
+
 class Intersection:
-  def __init__(self, streets, incoming, outgoing, num):
+  def __init__(self, incoming, outgoing, num):
     self.incoming = incoming
     self.outgoing = outgoing
-    self.streets = streets
     self.weights = None
     self.num = num
 
-def init_weights(self):
-	self.weights = np.ones(len(self.incoming))/len(incoming)
-	
-def get_cycle_from_weights(self, outfile):
-	self.max_cycle_len = 10
-	if(self.weights == None):
-		self.init_weights()
-	l = len(self.weights)
-	i = 0
-	while(i < l):
+  def init_weights(self):
+    self.weights = np.ones(len(self.incoming))/len(self.incoming)
+    
+  def get_cycle_from_weights(self, outfile):
+    self.max_cycle_len = 100000000
+    if(self.weights == None):
+      self.init_weights()
+    l = len(self.weights)
+    i = 0
+    while(i < l):
 
-		if self.weights[i] == 0:
-			self.weights.pop(i)
-			self.incoming.pop(i)
-			i -= 1
-			l-=1
-		i += 1
+      if self.weights[i] == 0:
+        self.weights.pop(i)
+        self.incoming.pop(i)
+        i -= 1
+        l-=1
+      i += 1
 
-	self.weights = np.array(weights)/sum(self.weights)
-	print(self.weights, self.incoming)
-	cycle_len = min(self.max_cycle_len, 1/min(self.weights))
+    self.weights = np.array(self.weights)/sum(self.weights)
+    cycle_len = min(self.max_cycle_len, 1/min(self.weights))
 
-	self.weights *= cycle_len
+    self.weights *= cycle_len
 
-	outfile.write(self.num)
-	outfile.write(len(weights))
-	for i in range(len(self.weights)):
-		outfile.write(self.incoming[i] + " " + str(int(self.weights[i])))
-
-
-if __name__ == '__main__':
-	weights()
+    outfile.write(str(self.num))
+    outfile.write('\n')
+    outfile.write(str(len(self.weights)))
+    outfile.write('\n')
+    for i in range(len(self.weights)):
+      outfile.write(self.incoming[i].name + " " + str(int(self.weights[i])))
+      outfile.write('\n')
