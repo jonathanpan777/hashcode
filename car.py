8b1c72b43bcb48114c4a763bf32cb3fc1e245bda
Srikar <srikar.rao001@gmail.com>
2021-02-25 12:17:28 -0800

    stuff

--- car.py
+++ car.py
@@ -1,13 +1,11 @@
 class Car:
-  def __init__(self, length, path):
-    self.length = length
-    self.path = path
-    self.current = path[0]
-    self.path_length = 0
-
-
-  def car_sum(street_dict):
-	s = 0
-	for p in path:
-		s += street_dict[p].length
-	c.path_length = 1/s 
+	def __init__(self, length, path):
+		self.length = length
+		self.path = path
+		self.current = path[0]
+		self.path_length = 0
+	def car_sum(street_dict):
+		s = 0
+		for p in path:
+			s += street_dict[p].length
+		c.path_length = 1/s 
--- intersection.py
+++ intersection.py
@@ -1,55 +1,48 @@
 import numpy as np
-
+import math
 class Intersection:
-  def __init__(self, incoming, outgoing, num):
-    self.incoming = incoming
-    self.outgoing = outgoing
-    self.weights = None
-    self.num = num
-    self.counts = [0] * len(incoming)
+	def __init__(self, incoming, outgoing, num):
+		self.incoming = incoming
+		self.outgoing = outgoing
+		self.weights = None
+		self.num = num
+		self.counts = [0] * len(incoming)
 
-def init_weights(self):
-	self.weights = np.ones(len(self.incoming))/len(incoming)
+	def init_weights(self):
+		self.weights = np.ones(len(self.incoming))/len(self.incoming)
 
-def set_weights(self, weights):
-	self.weights = weights
-	
-def get_cycle_from_weights(self, outfile):
-	self.max_cycle_len = 1000000000#can be changed later
-	if(self.weights == None):
-		self.init_weights()
-	l = len(self.weights)
-	i = 0
+	def set_weights(self, weights):
+		self.weights = self.weights
+		
+	def get_cycle_from_weights(self, outfile):
+		self.max_cycle_len = 1000000000#can be changed later
+		if(self.weights == None):
+			self.init_weights()
+		l = len(self.weights)
+		i = 0
 
-	while(i < l):
+		while(i < l):
 
-		if self.weights[i] == 0:
-			self.weights.pop(i)
-			self.incoming.pop(i)
-			i -= 1
-			l-=1
-		i += 1
+			if self.weights[i] == 0:
+				self.weights.pop(i)
+				self.incoming.pop(i)
+				i -= 1
+				l-=1
+			i += 1
 
-	self.weights = np.array(weights)/sum(self.weights)
-	print(self.weights, self.incoming)
-	cycle_len = min(self.max_cycle_len, 1/min(self.weights))
+			self.weights = np.array(self.weights)/sum(self.weights)
+			cycle_len = min(self.max_cycle_len, 1/min(self.weights))
 
-      if self.weights[i] == 0:
-        self.weights.pop(i)
-        self.incoming.pop(i)
-        i -= 1
-        l-=1
-      i += 1
 
-    self.weights = np.array(self.weights)/sum(self.weights)
-    cycle_len = min(self.max_cycle_len, 1/min(self.weights))
+		self.weights = np.array(self.weights)/sum(self.weights)
+		cycle_len = min(self.max_cycle_len, 1/min(self.weights))
 
-    self.weights *= cycle_len
+		self.weights *= cycle_len
 
-    outfile.write(str(self.num))
-    outfile.write('\n')
-    outfile.write(str(len(self.weights)))
-    outfile.write('\n')
-    for i in range(len(self.weights)):
-      outfile.write(self.incoming[i].name + " " + str(ceil(self.weights[i])))
-      outfile.write('\n')
+		outfile.write(str(self.num))
+		outfile.write('\n')
+		outfile.write(str(len(self.weights)))
+		outfile.write('\n')
+		for i in range(len(self.weights)):
+			  outfile.write(self.incoming[i].name + " " + str(math.ceil(self.weights[i])))
+			  outfile.write('\n')
